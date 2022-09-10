a3e8e7f874c2fce25d77a1afe2e40dc8bcec7a46
alex <alexjfuhr@gmail.com>
2022-09-09 23:45:18 -0400

    Added all instructions to the compiler Combined simple and argument instruction classes Added separate byte argument support to complex instruction class

--- compiler.py
+++ compiler.py
@@ -14,32 +14,29 @@ class Instruction():
         return "instruction"
 
 class SimpleInstruction(Instruction):
-    def __init__(self, _byte_code):
-        self.byte_code = _byte_code
-    def getInstructionType(self):
-        return "simpleInstruction"
-    def getByteCode(self):
-        return self.byte_code
-
-class SimpleArgumentInstruction(SimpleInstruction):
-    def __init__(self, _byte_code, _number_of_arguments):
+    def __init__(self, _byte_code, _number_of_arguments = 0):
         self.byte_code = _byte_code
         self.number_of_arguments = _number_of_arguments
-    def getInstructionType(self):
-        return "simpleArgumentInstruction"
+    def getByteCode(self):
+        return self.byte_code
     def getNumberOfArguments(self):
         return self.number_of_arguments
+    def getInstructionType(self):
+        return "simpleInstruction"
 
-class InByteArgumentInstruction(Instruction):
-    def __init__(self, _front_bits, _end_bits = "na"):
+class ComplexInstruction(Instruction):
+    def __init__(self, _front_bits, _end_bits = "na", _number_of_arguments = 0):
         self.front_bits = _front_bits
         self.end_bits = _end_bits
-    def getInstructionType(self):
-        return "inByteArgumentInstruction"
+        self.number_of_arguments = _number_of_arguments
     def getFrontBits(self):
         return self.front_bits
     def getEndBits(self):
         return self.end_bits
+    def getNumberOfArguments(self):
+        return self.number_of_arguments
+    def getInstructionType(self):
+        return "complexInstruction"
 
 class CustomByte(Instruction):
     def __init__(self):
@@ -56,100 +53,98 @@ class MoveInstruction(Instruction):
 instruction_dictionary = {
     ###### Command Instructions
     ### Input/Output Instructions
-    "IN" : SimpleArgumentInstruction(b'\xdb', 1),
-    "OUT" : SimpleArgumentInstruction(b'\xd3', 1),
+    "IN" : SimpleInstruction(b"\xdb", _number_of_arguments = 1),
+    "OUT" : SimpleInstruction(b"\xd3", _number_of_arguments = 1),
     ### Interrupt Instructions
-    "EI" : SimpleInstruction(b'\xfb'),
-    "DI" : SimpleInstruction(b'\xf3'),
-    "HLT" : SimpleInstruction(b'\x76'),
-    "RST" : InByteArgumentInstruction("11", _end_bits = "111"),
+    "EI" : SimpleInstruction(b"\xfb"),
+    "DI" : SimpleInstruction(b"\xf3"),
+    "HLT" : SimpleInstruction(b"\x76"),
+    "RST" : ComplexInstruction("11", _end_bits = "111"),
     ### Carry Bit Instructions
-    "CMC" : SimpleInstruction(b'\x3f'),
-    "STC" : SimpleInstruction(b'\x37'),
+    "CMC" : SimpleInstruction(b"\x3f"),
+    "STC" : SimpleInstruction(b"\x37"),
     ### No Operation Instruction
-    "NOP" : SimpleInstruction(b'\x00'),
+    "NOP" : SimpleInstruction(b"\x00"),
     ###### Single Register Instructions
-    #INR
-    #DCR
-    "CMA" : SimpleInstruction(b'\x2f'),
-    "DAA" : SimpleInstruction(b'\x27'),
+    "INR" : ComplexInstruction("00", _end_bits = "100"),
+    "DCR" : ComplexInstruction("00", _end_bits = "101"),
+    "CMA" : SimpleInstruction(b"\x2f"),
+    "DAA" : SimpleInstruction(b"\x27"),
     ###### Register Pair Instructions
-    #PUSH
-    #POP
-    #DAD
-    #INX
-    "INX" : InByteArgumentInstruction("00", _end_bits = "0011"),
-    #DCX
-    "XCHG" : SimpleInstruction(b'\xeb'),
-    "XTHL" : SimpleInstruction(b'\xe3'),
+    "PUSH" : ComplexInstruction("11", _end_bits = "0101"),
+    "POP" : ComplexInstruction("11", _end_bits = "0001"),
+    "DAD" : ComplexInstruction("00", _end_bits = "1001"),
+    "INX" : ComplexInstruction("00", _end_bits = "0011"),
+    "DCX" : ComplexInstruction("00", _end_bits = "1011"),
+    "XCHG" : SimpleInstruction(b"\xeb"),
+    "XTHL" : SimpleInstruction(b"\xe3"),
     ###### Rotate Accumulator Instructions
-    "RLC" : SimpleInstruction(b'\x07'),
-    "RRC" : SimpleInstruction(b'\x0f'),
-    "RAL" : SimpleInstruction(b'\x17'),
-    "RAR" : SimpleInstruction(b'\x1f'),
+    "RLC" : SimpleInstruction(b"\x07"),
+    "RRC" : SimpleInstruction(b"\x0f"),
+    "RAL" : SimpleInstruction(b"\x17"),
+    "RAR" : SimpleInstruction(b"\x1f"),
     ###### Data Transfer Instructions
     ### Data Transfer Instructions
     "MOV" : MoveInstruction(),
-    #STAX
-    #LDAX
+    "STAX" : ComplexInstruction("000", _end_bits = "0010"),
+    "LDAX" : ComplexInstruction("000", _end_bits = "1010"),
     ### Register/Memory to Accumulator Transfers
-    "ADD" : InByteArgumentInstruction("10000"),
-    #ADC
-    #SUB
-    #SBB
-    #ANA
-    #XRA
-    #ORA
-    #CMP
+    "ADD" : ComplexInstruction("10000"),
+    "ADC" : ComplexInstruction("10001"),
+    "SUB" : ComplexInstruction("10010"),
+    "SBB" : ComplexInstruction("10011"),
+    "ANA" : ComplexInstruction("10100"),
+    "XRA" : ComplexInstruction("10101"),
+    "ORA" : ComplexInstruction("10110"),
+    "CMP" : ComplexInstruction("10111"),
     ### Direct Addressing Instructions
-    "STA" : SimpleArgumentInstruction(b'\x32', 2),
-    "LDA" : SimpleArgumentInstruction(b'\x3a', 2),
-    "SHLD" : SimpleArgumentInstruction(b'\x22', 2),
-    "LHLD" : SimpleArgumentInstruction(b'\x2a', 2),
+    "STA" : SimpleInstruction(b"\x32", _number_of_arguments = 2),
+    "LDA" : SimpleInstruction(b"\x3a", _number_of_arguments = 2),
+    "SHLD" : SimpleInstruction(b"\x22", _number_of_arguments = 2),
+    "LHLD" : SimpleInstruction(b"\x2a", _number_of_arguments = 2),
     ###### Immediate Instructions
-    #LXI
-    #MVI
-    #ADI
-    #ACI
-    #SUI
-    #SBI
-    #ANI
-    #XRI
-    #ORI
-    #CPI
+    "LXI" : ComplexInstruction("00", _end_bits = "0001", _number_of_arguments = 1),
+    "MVI" : ComplexInstruction("00", _end_bits = "110", _number_of_arguments = 2),
+    "ADI" : SimpleInstruction(b"\xc6", _number_of_arguments = 1),
+    "ACI" : SimpleInstruction(b"\xce", _number_of_arguments = 1),
+    "SUI" : SimpleInstruction(b"\xd6", _number_of_arguments = 1),
+    "SBI" : SimpleInstruction(b"\xde", _number_of_arguments = 1),
+    "ANI" : SimpleInstruction(b"\xe6", _number_of_arguments = 1),
+    "XRI" : SimpleInstruction(b"\xee", _number_of_arguments = 1),
+    "ORI" : SimpleInstruction(b"\xf6", _number_of_arguments = 1),
+    "CPI" : SimpleInstruction(b"\xfe", _number_of_arguments = 1),
     ###### Branching Instructions
     ### Jump Instructions
-    #PCHL
-    "JMP" : SimpleArgumentInstruction(b'\xc3', 2),
-    #JC
-    #JNC
-    #JZ
-    #JNZ
-    #JM
-    #JP
-    #JPE
-    #JPO
+    "PCHL" : SimpleInstruction(b"\xe9"),
+    "JMP" : SimpleInstruction(b"\xc3", _number_of_arguments = 2),
+    "JC" : SimpleInstruction(b"\xda", _number_of_arguments = 2),
+    "JNC" : SimpleInstruction(b"\xd2", _number_of_arguments = 2),
+    "JZ" : SimpleInstruction(b"\xca", _number_of_arguments = 2),
+    "JNZ" : SimpleInstruction(b"\xc2", _number_of_arguments = 2),
+    "JM" : SimpleInstruction(b"\xfa", _number_of_arguments = 2),
+    "JP" : SimpleInstruction(b"\xf2", _number_of_arguments = 2),
+    "JPE" : SimpleInstruction(b"\xea", _number_of_arguments = 2),
+    "JPO" : SimpleInstruction(b"\xe2", _number_of_arguments = 2),
     ### Call Instructions
-    #CALL
-    #CC
-    #CNC
-    #CZ
-    #CNZ
-    #CM
-    #CP
-    #CPE
-    #CPO
+    "CALL" : SimpleInstruction(b"\xcd", _number_of_arguments = 2),
+    "CC" : SimpleInstruction(b"\xdc", _number_of_arguments = 2),
+    "CNC" : SimpleInstruction(b"\xd4", _number_of_arguments = 2),
+    "CZ" : SimpleInstruction(b"\xcc", _number_of_arguments = 2),
+    "CNZ" : SimpleInstruction(b"\xc4", _number_of_arguments = 2),
+    "CM" : SimpleInstruction(b"\xfc", _number_of_arguments = 2),
+    "CP" : SimpleInstruction(b"\xf4", _number_of_arguments = 2),
+    "CPE" : SimpleInstruction(b"\xec", _number_of_arguments = 2),
+    "CPO" : SimpleInstruction(b"\xe4", _number_of_arguments = 2),
     ### Return Instructions
-    #RET
-    #RC
-    #RNC
-    #RZ
-    #RNZ
-    #RM
-    #RP
-    #RPE
-    #RPO
-
+    "RET" : SimpleInstruction(b"\xc9"),
+    "RC" : SimpleInstruction(b"\xd8"),
+    "RNC" : SimpleInstruction(b"\xd0"),
+    "RZ" : SimpleInstruction(b"\xc8"),
+    "RNZ" : SimpleInstruction(b"\xc0"),
+    "RM" : SimpleInstruction(b"\xf8"),
+    "RP" : SimpleInstruction(b"\xf0"),
+    "RPE" : SimpleInstruction(b"\xe8"),
+    "RPO" : SimpleInstruction(b"\xe0"),
 
     ###### Custom Byte
     ### This is not an Altair instruction. It is for bytes to be placed at the end of a program that contain needed data.
@@ -168,7 +163,7 @@ single_register_dictionary = {
 }
 
 file = open(file_location, "r")
-compiled_bytes = b''
+compiled_bytes = b""
 
 for line in file:
     print(line[:-1])
@@ -187,15 +182,11 @@ for line in file:
     if instruction_type == "simpleInstruction":
         byte_code = instruction.getByteCode()
         compiled_bytes += byte_code
-
-    elif instruction_type == "simpleArgumentInstruction":
-        byte_code = instruction.getByteCode()
-        compiled_bytes += byte_code
         for argument in range(1, instruction.getNumberOfArguments() + 1):
             argument_byte = line_split[argument]
             compiled_bytes += bytes.fromhex(argument_byte)
 
-    elif instruction_type == "inByteArgumentInstruction":
+    elif instruction_type == "complexInstruction":
         front_bits = instruction.getFrontBits()
         end_bits = instruction.getEndBits()
         argument_bits = line_split[1]
@@ -209,6 +200,9 @@ for line in file:
             end_bits = int(end_bits, 2)
             byte_code = (front_bits + argument_bits + end_bits).to_bytes(1, "little")
         compiled_bytes += byte_code
+        for argument in range(1, instruction.getNumberOfArguments() + 1):
+            argument_byte = line_split[argument]
+            compiled_bytes += bytes.fromhex(argument_byte)
 
     elif instruction_type == "customByte":
         for data_byte in line_split[1:]:
