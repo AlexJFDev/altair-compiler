7266d06cb6358dc218bd12b724980ec7ba51271e
AlexJFDev <alexjfuhr@gmail.com>
2022-09-19 11:37:09 -0400

    ** Version 1.1.2 ** Fixed debugs

--- src/main.py
+++ src/main.py
@@ -13,18 +13,17 @@ if __name__ == "__main__":
         sys.exit()
     elif len(sys.argv) == 2:
         assembler = Assembler(sys.argv[1], False)
-        assembler.assemble()
-        assembler.write()
     elif len(sys.argv) == 3:
-        assembler = Assembler(sys.argv[1], False, _output_file_location = sys.argv[2])
-        assembler.assemble()
-        assembler.write()
+        if sys.argv[2] == "-print-debugs":
+            assembler = Assembler(sys.argv[1], True)
+        else:
+            assembler = Assembler(sys.argv[1], False, _output_file_location = sys.argv[2])
     elif len(sys.argv) >= 4:
-        if sys.argv[3] == "print-debugs":
+        if sys.argv[3] == "-print-debugs":
             assembler = Assembler(sys.argv[1], True, _output_file_location = sys.argv[2])
         else:
-            assembler = Assembler(sys.argv[1], True, _output_file_location = sys.argv[2])
-        assembler.assemble()
-        assembler.write()
+            assembler = Assembler(sys.argv[1], False, _output_file_location = sys.argv[2])
+    assembler.assemble()
+    assembler.write()
     if len(sys.argv) >4:
         print("extra arguments were provided and ignored")
